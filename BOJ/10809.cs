aa19fbe239447d3fa731cc271cd8d5ab7cfea538
Claude Agnes <rlawnstjddl12@gmail.com>
2022-08-25 22:16:39 +0900

    Add Files
    BOJ Problem Solved

---
+++ BOJ/10809.cs
@@ -0,0 +1,23 @@
+﻿namespace Study
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = Console.ReadLine();
+
+            for (int i = 97; i < 123; i++)
+            {
+                if (s.Contains(Convert.ToChar(i)))
+                {
+                    Console.Write(s.IndexOf(Convert.ToChar(i)));
+                    Console.Write(" ");
+                }
+                else
+                {
+                    Console.Write("-1 ");
+                }
+            }
+        }
+    }
+}
---
+++ BOJ/2475.cs
@@ -0,0 +1,17 @@
+﻿namespace Study
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] num = Console.ReadLine().Split();
+            int a = 0;
+            for(int i = 0; i <num.Length; i++)
+            {
+                a += int.Parse(num[i]) * int.Parse(num[i]);
+            }
+            int result = a % 10;
+            Console.WriteLine(result);
+        }
+    }
+}
