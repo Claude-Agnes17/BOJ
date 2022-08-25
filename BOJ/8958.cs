d6a86a93cac0974b3615168a7384a45193d8ea34
Claude Agnes <rlawnstjddl12@gmail.com>
2022-08-25 20:36:12 +0900

    Create 8958.cs

---
+++ BOJ/8958.cs
@@ -0,0 +1,40 @@
+﻿namespace Study
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            string[] Case = new string[n];
+            for(int i = 0; i < n; i++)
+            {
+                Case[i] = Console.ReadLine();
+            }
+
+            for(int i = 0; i < n; i++)
+            {
+                char[] temp = Case[i].ToCharArray();
+                int j = 0;
+                int resultScore = 0;
+                int score = 0;
+
+                while (true)
+                {
+                    if(temp[j].ToString() == "O")
+                    {
+                        score++;
+                        resultScore += score;
+                    }
+                    else
+                    {
+                        score = 0;
+                    }
+                    j++;
+                    if (j == temp.Length) break;
+                }
+                Console.WriteLine(resultScore);
+            }
+        }
+    }
+}
