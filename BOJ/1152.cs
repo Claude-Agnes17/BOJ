a578e3e4d635e5fca03a726f745e03fca1006933
Claude Agnes <rlawnstjddl12@gmail.com>
2022-08-26 14:06:36 +0900

    Create 1152.cs

---
+++ BOJ/1152.cs
@@ -0,0 +1,28 @@
+﻿namespace Study
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 0;
+
+            string s = Console.ReadLine().Trim();
+
+            for(int i=0; i<s.Length; i++)
+            {
+                if (s[i] == ' ')
+                {
+                    a++;
+                }
+            }
+            if(s.Length == 0)
+            {
+                Console.WriteLine(0);
+            }
+            else
+            {
+                Console.WriteLine(a+1);
+            }
+        }
+    }
+}
