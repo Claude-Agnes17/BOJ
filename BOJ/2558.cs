29b08eedb743aabadc549559d5b047829fe7751e
Claude Agnes <rlawnstjddl12@gmail.com>
2022-08-31 17:23:00 +0900

    Create 2558.cs

---
+++ BOJ/2558.cs
@@ -0,0 +1,13 @@
+﻿namespace Study
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(a + b);
+        }
+    }
+}
