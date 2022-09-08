62d6605ecd7bce38762b095daa4044656e898719
Claude Agnes <rlawnstjddl12@gmail.com>
2022-09-08 11:37:05 +0900

    Create 10699.cs

---
+++ BOJ/10699.cs
@@ -0,0 +1,11 @@
+﻿
+namespace Study
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(DateTime.Today.ToString("yyyy-MM-dd"));
+        }
+    }
+}
